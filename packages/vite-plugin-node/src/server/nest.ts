a379af1ad901caffaee2c44d223930346d0f9ee8
Filyus <fahmedyanov@dpg.email>
2022-12-28 15:47:12 +0500

    Check if the Nest app is already initialized

--- packages/vite-plugin-node/src/server/nest.ts
+++ packages/vite-plugin-node/src/server/nest.ts
@@ -2,7 +2,9 @@ import type { INestApplication } from '@nestjs/common';
 import type { RequestAdapter } from '..';
 
 export const NestHandler: RequestAdapter<INestApplication> = async ({ app, req, res }) => {
-  await app.init();
+  if (!app.isInitialized) {
+    await app.init();
+  }
   const instance = app.getHttpAdapter().getInstance();
 
   // Todo: handle nest-fastify case
